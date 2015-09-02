94f6e7f7bd6abdcaabd86d17f36d61bc4d7eed10
erik <erik.moqvist@gmail.com>
2015-09-02 23:00:13 +0200

    version 0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='cantools',
-      version='0.2.1',
+      version='0.2.2',
       description='CAN BUS tools.',
       long_description=open('README.rst', 'r').read(),
       author='Erik Moqvist',

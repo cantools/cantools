4cdc357ba1980dbe991d22dd118ecb4a7ff8e5e9
erik <erik.moqvist@gmail.com>
2015-09-17 07:18:49 +0200

    1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='bitstruct',
-      version='0.7.2',
+      version='1.0.0',
       description=('This module performs conversions between Python values '
                    'and C bit field structs represented as Python '
                    'bytearrays.'),

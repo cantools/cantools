2347d1a82002cf8827c5f29e1cf9db7a3b6d0659
Erik Moqvist <erik.moqvist@gmail.com>
2018-05-30 22:17:32 +0200

    Test the KCD dump function (not implemented).

--- tests/test_database.py
+++ tests/test_database.py
@@ -676,6 +676,12 @@ IO_DEBUG(
         filename = os.path.join('tests', 'files', 'the_homer.kcd')
         db = cantools.db.load_file(filename)
 
+        with self.assertRaises(NotImplementedError) as cm:
+            db.as_kcd_string()
+
+        self.assertEqual(str(cm.exception),
+                         'The KCD dump function is not yet implemented.')
+
         self.assertEqual(db.version, '1.23')
         self.assertEqual(len(db.nodes), 18)
         self.assertEqual(db.nodes[0].name, 'Motor ACME')

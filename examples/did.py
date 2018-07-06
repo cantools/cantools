4177e812409d490d929b3994890efff48db710c4
Erik Moqvist <erik.moqvist@gmail.com>
2018-07-06 22:26:41 +0200

    DID example.

---
+++ examples/did.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+#
+# > python3 did.py
+# Data:    {'Country_variant': 'Europe', 'Vehicle_type': 'Sedan', 'Special_setting': 3}
+# Encoded: 2103
+# Decoded: {'Country_variant': 'Europe', 'Vehicle_type': 'Sedan', 'Special_setting': 3}
+#
+
+from __future__ import print_function
+import os
+from binascii import hexlify
+import cantools
+
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+EXAMPLE_PATH = os.path.join(SCRIPT_DIR,
+                            '..',
+                            'tests',
+                            'files',
+                            'example.cdd')
+
+database = cantools.db.load_file(EXAMPLE_PATH)
+
+data = {
+    'Country_variant': 'Europe',
+    'Vehicle_type': 'Sedan',
+    'Special_setting': 3
+}
+
+did = database.get_did_by_name('Coding')
+encoded = did.encode(data)
+decoded = did.decode(encoded)
+
+print('Data:   ', data)
+print('Encoded:', hexlify(encoded).decode('ascii'))
+print('Decoded:', decoded)

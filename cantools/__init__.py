41e2605d014f41425d950c2885732cf31bc80eeb
Erik Moqvist <erik.moqvist@gmail.com>
2018-07-09 18:44:32 +0200

    Version 23.1.0.

--- cantools/__init__.py
+++ cantools/__init__.py
@@ -8,7 +8,7 @@ from . import tester
 
 
 __author__ = 'Erik Moqvist'
-__version__ = '23.0.0'
+__version__ = '23.1.0'
 
 
 # Matches 'candump' output, i.e. "vcan0  1F0   [8]  00 00 00 00 00 00 1B C1".

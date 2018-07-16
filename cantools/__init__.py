ab4281054a29749a1101b79df5e3c999fd084de5
Erik Moqvist <erik.moqvist@gmail.com>
2018-07-16 08:00:25 +0200

    Version 27.1.0.

--- cantools/__init__.py
+++ cantools/__init__.py
@@ -13,7 +13,7 @@ from . import database as db
 
 
 __author__ = 'Erik Moqvist'
-__version__ = '27.0.0'
+__version__ = '27.1.0'
 
 
 # Matches 'candump' output, i.e. "vcan0  1F0   [8]  00 00 00 00 00 00 1B C1".

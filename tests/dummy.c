084a0e41f9344725f815259b1fd4099ce7d4d3ce
Karl <karlding@users.noreply.github.com>
2018-12-01 10:45:42 -0800

    Use Allman-style braces

--- tests/dummy.c
+++ tests/dummy.c
@@ -3,7 +3,8 @@
  * CFLAGS can be added. It is imperative that this always will compile
  * correctly, in order to give accurate results.
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   (void)argc;
   (void)argv;
 

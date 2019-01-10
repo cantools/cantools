61fdde862e118034b133c9e60169ba0bb25288d5
Erik Moqvist <erik.moqvist@gmail.com>
2019-01-10 07:28:07 +0100

    Travis debug.

--- tests/test_command_line.py
+++ tests/test_command_line.py
@@ -658,6 +658,7 @@ IO_DEBUG(
                              read_file(database_c))
             self.assertEqual(read_file('tests/files/c_source/' + fuzzer_c),
                              read_file(fuzzer_c))
+            print(os.getcwd())
             self.assertEqual(read_file('tests/files/c_source/' + fuzzer_mk),
                              read_file(fuzzer_mk))
 

6021f6b68d03f5db765def199292791388e55301
Erik Moqvist <erik.moqvist@gmail.com>
2019-01-12 21:49:06 +0100

    More monitor tests.

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -10,17 +10,20 @@ except ImportError:
     from mock import patch
     from mock import call
 
+import can
 from cantools.subparsers.monitor import Monitor
 
 
 class Args(object):
 
-    def __init__(self, database):
+    def __init__(self,
+                 database,
+                 single_line=False):
         self.database = database
         self.encoding = None
         self.frame_id_mask = None
         self.no_strict = False
-        self.single_line = False
+        self.single_line = single_line
         self.bit_rate = None
         self.bus_type = 'socketcan'
         self.channel = 'vcan0'
@@ -91,6 +94,102 @@ class CanToolsMonitorTest(unittest.TestCase):
                      0,
                      '   TIMESTAMP  MESSAGE                                           ',
                      'green'),
+                call(29,
+                     0,
+                     'q: Quit, f: Filter, p: Play/Pause, r: Reset                     ',
+                     'cyan')
+            ])
+
+    @patch('can.Notifier')
+    @patch('can.Bus')
+    @patch('curses.color_pair')
+    @patch('curses.is_term_resized')
+    @patch('curses.init_pair')
+    @patch('curses.curs_set')
+    @patch('curses.use_default_colors')
+    def test_display_one_frame_quit(self,
+                                    _use_default_colors,
+                                    _curs_set,
+                                    _init_pair,
+                                    is_term_resized,
+                                    color_pair,
+                                    _bus,
+                                    _notifier):
+        # Prepare mocks.
+        stdscr = StdScr()
+        args = Args('tests/files/motohawk.dbc')
+        color_pair.side_effect = ['green', 'cyan']
+        is_term_resized.return_value = False
+
+        # Run monitor.
+        monitor = Monitor(stdscr, args)
+        monitor.on_message_received(can.Message(
+            arbitration_id=496,
+            data=b'\xc0\x06\xe0\x00\x00\x00\x00\x00'))
+        monitor.run()
+
+        # Check mocks.
+        self.assert_called(
+            stdscr.addstr,
+            [
+                call(0, 0, 'Received: 1, Discarded: 0, Errors: 0'),
+                call(1,
+                     0,
+                     '   TIMESTAMP  MESSAGE                                           ',
+                     'green'),
+                call(2, 0, '       0.000  ExampleMessage('),
+                call(3, 0, "                  Enable: 'Enabled' -,"),
+                call(4, 0, '                  AverageRadius: 3.2 m,'),
+                call(5, 0, '                  Temperature: 250.55 degK'),
+                call(6, 0, '              )'),
+                call(29,
+                     0,
+                     'q: Quit, f: Filter, p: Play/Pause, r: Reset                     ',
+                     'cyan')
+            ])
+
+    @patch('can.Notifier')
+    @patch('can.Bus')
+    @patch('curses.color_pair')
+    @patch('curses.is_term_resized')
+    @patch('curses.init_pair')
+    @patch('curses.curs_set')
+    @patch('curses.use_default_colors')
+    def test_display_one_frame_single_line_quit(self,
+                                                _use_default_colors,
+                                                _curs_set,
+                                                _init_pair,
+                                                is_term_resized,
+                                                color_pair,
+                                                _bus,
+                                                _notifier):
+        # Prepare mocks.
+        stdscr = StdScr()
+        args = Args('tests/files/motohawk.dbc',
+                    single_line=True)
+        color_pair.side_effect = ['green', 'cyan']
+        is_term_resized.return_value = False
+
+        # Run monitor.
+        monitor = Monitor(stdscr, args)
+        monitor.on_message_received(can.Message(
+            arbitration_id=496,
+            data=b'\xc0\x06\xe0\x00\x00\x00\x00\x00'))
+        monitor.run()
+
+        # Check mocks.
+        self.assert_called(
+            stdscr.addstr,
+            [
+                call(0, 0, 'Received: 1, Discarded: 0, Errors: 0'),
+                call(1,
+                     0,
+                     '   TIMESTAMP  MESSAGE                                           ',
+                     'green'),
+                call(2,
+                     0,
+                     "       0.000  ExampleMessage(Enable: 'Enabled' -, "
+                     "AverageRadius: 3.2 m, Temperature: 250.55 degK)"),
                 call(29,
                      0,
                      'q: Quit, f: Filter, p: Play/Pause, r: Reset                     ',

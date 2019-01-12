65d4540f16c0686acdb636b3ae1d1a7b48bc43b7
Erik Moqvist <erik.moqvist@gmail.com>
2019-01-12 21:53:48 +0100

    More monitor tests.

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -196,6 +196,59 @@ class CanToolsMonitorTest(unittest.TestCase):
                      'cyan')
             ])
 
+    @patch('can.Notifier')
+    @patch('can.Bus')
+    @patch('curses.color_pair')
+    @patch('curses.is_term_resized')
+    @patch('curses.init_pair')
+    @patch('curses.curs_set')
+    @patch('curses.use_default_colors')
+    def test_display_one_frame_input_twice_quit(self,
+                                                _use_default_colors,
+                                                _curs_set,
+                                                _init_pair,
+                                                is_term_resized,
+                                                color_pair,
+                                                _bus,
+                                                _notifier):
+        # Prepare mocks.
+        stdscr = StdScr()
+        args = Args('tests/files/motohawk.dbc')
+        color_pair.side_effect = ['green', 'cyan']
+        is_term_resized.return_value = False
+
+        # Run monitor.
+        monitor = Monitor(stdscr, args)
+        monitor.on_message_received(can.Message(
+            arbitration_id=496,
+            data=b'\xc0\x06\xe0\x00\x00\x00\x00\x00',
+            timestamp=1.0))
+        monitor.on_message_received(can.Message(
+            arbitration_id=496,
+            data=b'\xc0\x06\xd0\x00\x00\x00\x00\x00',
+            timestamp=2.1))
+        monitor.run()
+
+        # Check mocks.
+        self.assert_called(
+            stdscr.addstr,
+            [
+                call(0, 0, 'Received: 2, Discarded: 0, Errors: 0'),
+                call(1,
+                     0,
+                     '   TIMESTAMP  MESSAGE                                           ',
+                     'green'),
+                call(2, 0, '       1.100  ExampleMessage('),
+                call(3, 0, "                  Enable: 'Enabled' -,"),
+                call(4, 0, '                  AverageRadius: 3.2 m,'),
+                call(5, 0, '                  Temperature: 250.54 degK'),
+                call(6, 0, '              )'),
+                call(29,
+                     0,
+                     'q: Quit, f: Filter, p: Play/Pause, r: Reset                     ',
+                     'cyan')
+            ])
+
 
 if __name__ == '__main__':
     unittest.main()

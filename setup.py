1b009b0075a1ebd014a51ce93c588a3ae7f4084e
Kyle Altendorf <sda@fstab.net>
2021-02-03 10:49:25 -0500

    add py3 only classifier

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(name='cantools',
       classifiers=[
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3 :: Only',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',

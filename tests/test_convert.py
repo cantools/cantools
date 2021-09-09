28023ad9de314d1d677b28b1a062d48801397106
erzoe <erzo@posteo.de>
2021-09-09 14:04:24 +0200

    test multiplex

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -15,6 +15,7 @@ import cantools
 class CanToolsConvertFullTest(unittest.TestCase):
 
     DBC_FILE_CHOICES = os.path.join(os.path.split(__file__)[0], 'files/dbc/choices.dbc')
+    DBC_FILE_MULTIPLEX = os.path.join(os.path.split(__file__)[0], 'files/dbc/multiplex_2.dbc')
 
     reo_lines_that_are_expected_to_differ = re.compile(
             r'^\\newcommand{\\(?P<key0>infile|outfile)}{(?P<val0>.*?)}|'
@@ -82,7 +83,7 @@ class CanToolsConvertFullTest(unittest.TestCase):
         return s[:i0] + val + s[i1:]
 
 
-    # ------- test cases -------
+    # ------- test choices -------
 
     def test_choices(self):
         dbc = self.DBC_FILE_CHOICES
@@ -190,6 +191,248 @@ DLC = 8
 	\quad\textbullet~0 & With space \\
 	\quad\textbullet~-5 & A negative value \\
 	\end{tabular}
+
+\end{description}
+
+\end{document}
+'''.lstrip('\n')
+
+        with mock.patch('sys.argv', argv):
+            cantools._main()
+
+        self.assert_file_content_equal(expected_content, fn_out)
+
+
+    # ------- test multiplex -------
+
+    def test_multiplex(self):
+        dbc = self.DBC_FILE_MULTIPLEX
+        fn_out = self.get_out_file_name(dbc)
+        argv = ['cantools', 'convert', dbc, fn_out]
+
+        expected_content = r'''
+% !TeX program = pdflatex
+\documentclass[a4paper]{article}
+
+\usepackage{typearea}
+\usepackage{parskip}
+\usepackage{booktabs}
+\usepackage{siunitx}
+\usepackage{fancyhdr}
+\usepackage{lastpage}
+\usepackage{hyperref}
+
+\hypersetup{hidelinks}
+\setcounter{secnumdepth}{0}
+
+\providecommand{\degree}{\ensuremath{^\circ}}
+\newcommand{\thead}[1]{#1}
+
+\makeatletter
+    \newcommand{\thetitle}{\@title}
+    \newcommand{\thedate}{\@date}
+    \newcommand{\theauthor}{\@author}
+\makeatother
+\renewcommand{\maketitle}{%
+    \begin{center}%
+        \Large
+        \thetitle
+    \end{center}%
+}
+
+\iffalse
+	\usepackage[table]{xcolor}
+	\catcode`*=\active
+	\def*{\rowcolor{green!20}}
+\fi
+
+\usepackage{xltabular}
+
+\KOMAoption{DIV}{12}
+
+\title{Multiplex 2}
+\date{09.09.2021}
+\newcommand{\infile}{multiplex\_2.dbc}
+\newcommand{\outfile}{multiplex\_2.tex}
+
+\fancyhead{}
+\fancyhead[ol,er]{}
+\fancyhead[or,el]{}
+\renewcommand{\headrulewidth}{0pt}
+\fancyfoot{}
+\fancyfoot[ol,er]{Created from \infile, \thedate}
+\fancyfoot[or,el]{Page \thepage\ of \pageref{LastPage}}
+\pagestyle{fancy}
+
+
+\newcommand{\sig}[1]{{\def\-{\discretionary{-}{\hbox{\footnotesize$\hookrightarrow$ }}{}}#1}}
+
+\begin{document}
+\maketitle
+
+\section{0xC00FEFE Extended}
+Extended frame \\
+DLC = 8
+
+\begingroup
+\centering
+\begin{xltabular}{1\linewidth}{XclS[table-format=2.0]S[table-format=2.0]clcS[table-format=1.0]S[table-format=1.0]lS[table-format=4.0]S[table-format=4.0]c}
+	\toprule
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endfirsthead
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endhead
+	\midrule
+		\multicolumn{\expandafter\the\csname LT@cols\endcsname}{r}{(continued on next page)} \\
+\endfoot
+	\bottomrule
+\endlastfoot
+
+	\sig{S0} & M && 0 & 4 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S1} & m0M && 4 & 4 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S5} & m1 && 4 & 28 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S2} & m0 && 8 & 8 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S4} & m2 && 8 & 24 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S3} & m0 && 16 & 16 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S6} & M && 32 & 8 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S7} & m1 && 40 & 24 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S8} & m2 && 40 & 8 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+\end{xltabular}
+\par
+\endgroup
+
+
+%\subsection{Comments, allowed values and multiplexing details}
+\begin{description}
+\item[\sig{S1}] This signal is available only if S0 has the value 0x0.
+
+\item[\sig{S5}] This signal is available only if S0 has the value 0x1.
+
+\item[\sig{S2}] This signal is available only if S1 has the value 0x0.
+
+\item[\sig{S4}] This signal is available only if S1 has the value 0x2.
+
+\item[\sig{S3}] This signal is available only if S1 has the value 0x0.
+
+\item[\sig{S7}] This signal is available only if S6 has the value 0x1.
+
+\item[\sig{S8}] This signal is available only if S6 has the value 0x2.
+
+\end{description}
+
+
+\section{0xC01FEFE Normal}
+Extended frame \\
+DLC = 8
+
+\begingroup
+\centering
+\begin{xltabular}{1\linewidth}{XclS[table-format=1.0]S[table-format=1.0]clcS[table-format=1.0]S[table-format=1.0]lS[table-format=4.0]S[table-format=4.0]c}
+	\toprule
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endfirsthead
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endhead
+	\midrule
+		\multicolumn{\expandafter\the\csname LT@cols\endcsname}{r}{(continued on next page)} \\
+\endfoot
+	\bottomrule
+\endlastfoot
+
+	\sig{S0} & M && 0 & 4 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S1} & m0 && 4 & 4 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S2} & m1 && 8 & 8 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+\end{xltabular}
+\par
+\endgroup
+
+
+%\subsection{Comments, allowed values and multiplexing details}
+\begin{description}
+\item[\sig{S1}] This signal is available only if S0 has the value 0x0.
+
+\item[\sig{S2}] This signal is available only if S0 has the value 0x1.
+
+\end{description}
+
+
+\section{0xC02FEFE Shared}
+Extended frame \\
+DLC = 8
+
+\begingroup
+\centering
+\begin{xltabular}{1\linewidth}{XclS[table-format=1.0]S[table-format=1.0]clcS[table-format=1.0]S[table-format=1.0]lS[table-format=4.0]S[table-format=4.0]c}
+	\toprule
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endfirsthead
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endhead
+	\midrule
+		\multicolumn{\expandafter\the\csname LT@cols\endcsname}{r}{(continued on next page)} \\
+\endfoot
+	\bottomrule
+\endlastfoot
+
+	\sig{S0} & M && 0 & 4 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S1} & mx && 4 & 4 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S2} & mx && 8 & 8 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+\end{xltabular}
+\par
+\endgroup
+
+
+%\subsection{Comments, allowed values and multiplexing details}
+\begin{description}
+\item[\sig{S1}] This signal is available only if S0 has one of the values 0x1 or 0x3.
+
+\item[\sig{S2}] This signal is available only if S0 has one of the values 0x2, 0x3, 0x4 or 0x5.
+
+\end{description}
+
+
+\section{0xC03FEFE ExtendedTypes}
+Extended frame \\
+DLC = 8
+
+\begingroup
+\centering
+\begin{xltabular}{1\linewidth}{XclS[table-format=2.0]S[table-format=2.0]clcS[table-format=1.0]S[table-format=1.0]lS[table-format=4.2E3]S[table-format=4.1E3]c}
+	\toprule
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endfirsthead
+		{\thead{Name}} & {\thead{Mux}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endhead
+	\midrule
+		\multicolumn{\expandafter\the\csname LT@cols\endcsname}{r}{(continued on next page)} \\
+\endfoot
+	\bottomrule
+\endlastfoot
+
+	\sig{S11} & M && 0 & 5 & LE && uint & 1 & 0 && {\texttt{0x02}} & {\texttt{0x06}} & {--} \\
+	\sig{S0} & m3M && 8 & 4 & LE && int & 1 & 0 && {--} & {--} & {--} \\
+	\sig{S10} & m0 && 16 & 32 & LE && float & 1 & 0 && -3.4E+38 & 3.4E+38 & {--} \\
+	\sig{S9} & m5 && 24 & 32 & LE && float & 1 & 0 && -1.34 & 1235 & {--} \\
+\end{xltabular}
+\par
+\endgroup
+
+
+%\subsection{Comments, allowed values and multiplexing details}
+\begin{description}
+\item[\sig{S0}] This signal is available only if S11 has the value 0x3.
+
+\item[\sig{S10}] This signal is available only if S0 has the value 0x0.
+
+\item[\sig{S9}] This signal is available only if S11 has the value 0x5.
 
 \end{description}
 

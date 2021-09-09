d493dc8e123bb4714ecdb75dea07ce0db7dd1d17
erzoe <erzo@posteo.de>
2021-09-09 12:24:45 +0200

    no separate file for expected content
    bugfix for commit 1aff3e4a9d9bcf5f6eb6aea15ffded1bdcca5521
    
    this worked great on my machine, but not on github.
    It did not find the tex file, so I've embedded the expected LaTeX code into the python test program.

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -38,13 +38,12 @@ class CanToolsConvertFullTest(unittest.TestCase):
     def get_golden_master_file_name(self, fn_dbc):
         return os.path.splitext(fn_dbc)[0] + '.tex'
 
-    def assert_file_content_equal(self, fn_expected, fn_is):
-        read_is = lambda f: [self.reo_lines_that_are_expected_to_differ.sub(self.sub_line, ln) for ln in f]
+    def assert_file_content_equal(self, content_expected, fn_is):
+        read_is = lambda f: [self.reo_lines_that_are_expected_to_differ.sub(self.sub_line, ln).rstrip('\n') for ln in f]
 
         with open(fn_is, 'rt') as f:
             content_is = read_is(f)
-        with open(fn_expected, 'rt') as f:
-            content_expected = list(f)
+        content_expected = content_expected.splitlines()
         self.assertEqual(content_expected, content_is)
 
     def sub_line(self, m):
@@ -85,13 +84,119 @@ class CanToolsConvertFullTest(unittest.TestCase):
     def test_choices(self):
         dbc = self.DBC_FILE_CHOICES
         fn_out = self.get_out_file_name(dbc)
-        fn_golden_master = self.get_golden_master_file_name(dbc)
         argv = ['cantools', 'convert', dbc, fn_out]
+
+        expected_content = r'''
+% !TeX program = pdflatex
+\documentclass[a4paper]{article}
+
+\usepackage{typearea}
+\usepackage{parskip}
+\usepackage{booktabs}
+\usepackage{siunitx}
+\usepackage{fancyhdr}
+\usepackage{lastpage}
+\usepackage{hyperref}
+
+\hypersetup{hidelinks}
+\setcounter{secnumdepth}{0}
+
+\providecommand{\degree}{\ensuremath{^\circ}}
+\newcommand{\thead}[1]{#1}
+
+\makeatletter
+    \newcommand{\thetitle}{\@title}
+    \newcommand{\thedate}{\@date}
+    \newcommand{\theauthor}{\@author}
+\makeatother
+\renewcommand{\maketitle}{%
+    \begin{center}%
+        \Large
+        \thetitle
+    \end{center}%
+}
+
+\iffalse
+	\usepackage[table]{xcolor}
+	\catcode`*=\active
+	\def*{\rowcolor{green!20}}
+\fi
+
+\usepackage{xltabular}
+
+\KOMAoption{DIV}{12}
+
+\title{Choices}
+\date{09.09.2021}
+\newcommand{\infile}{choices.dbc}
+\newcommand{\outfile}{choices.tex}
+
+\fancyhead{}
+\fancyhead[ol,er]{}
+\fancyhead[or,el]{}
+\renewcommand{\headrulewidth}{0pt}
+\fancyfoot{}
+\fancyfoot[ol,er]{Created from \infile, \thedate}
+\fancyfoot[or,el]{Page \thepage\ of \pageref{LastPage}}
+\pagestyle{fancy}
+
+
+\newcommand{\sig}[1]{{\def\-{\discretionary{-}{\hbox{\footnotesize$\hookrightarrow$ }}{}}#1}}
+
+\begin{document}
+\maketitle
+
+\section{0x000 Foo}
+Base frame \\
+DLC = 8
+
+\begingroup
+\centering
+\begin{xltabular}{1\linewidth}{XlS[table-format=1.0]S[table-format=1.0]clcS[table-format=1.0]S[table-format=1.0]lS[table-format=4.0]S[table-format=3.0]c}
+	\toprule
+		{\thead{Name}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endfirsthead
+		{\thead{Name}} && {\thead{Start}} & {\thead{Bits}} & {\thead{E}} && {\thead{Type}} & {\thead{Scale}} & {\thead{Offset}} && {\thead{Min}} & {\thead{Max}} & {\thead{Unit}} \\
+	\midrule
+\endhead
+	\midrule
+		\multicolumn{\expandafter\the\csname LT@cols\endcsname}{r}{(continued on next page)} \\
+\endfoot
+	\bottomrule
+\endlastfoot
+
+	\sig{Foo} && 0 & 8 & LE && int & 1 & 0 && -128 & 127 & {--} \\
+\end{xltabular}
+\par
+\endgroup
+
+
+%\subsection{Comments, allowed values and multiplexing details}
+\begin{description}
+\item[\sig{Foo}] ~
+
+	\begin{tabular}{@{}l@{ }l}
+	\multicolumn{2}{@{}l}{Allowed values:} \\
+	\quad\textbullet~6 & reserved \\
+	\quad\textbullet~5 & reserved \\
+	\quad\textbullet~4 & unused 2 \\
+	\quad\textbullet~3 & unused \\
+	\quad\textbullet~2 & unused \\
+	\quad\textbullet~1 & \#\%=*ä' \\
+	\quad\textbullet~0 & With space \\
+	\quad\textbullet~-5 & A negative value \\
+	\end{tabular}
+
+\end{description}
+
+\end{document}
+'''.lstrip('\n')
 
         with mock.patch('sys.argv', argv):
             cantools._main()
 
-        self.assert_file_content_equal(fn_golden_master, fn_out)
+        self.assert_file_content_equal(expected_content, fn_out)
 
 
 if __name__ == '__main__':

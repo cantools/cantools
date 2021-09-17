f3f316dfb72b5a362f3f5a5d01ad804a608db417
erzoe <erzo@posteo.de>
2021-09-17 09:57:57 +0200

    test output of conversion from sym to dbc file

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -13,6 +13,7 @@ import cantools
 class CanToolsConvertFullTest(unittest.TestCase):
 
     SYM_FILE_COMMENTS_HEX_AND_MOTOROLA = os.path.join(os.path.split(__file__)[0], 'files', 'sym', 'comments_hex_and_motorola.sym')
+    DBC_FILE_COMMENTS_HEX_AND_MOTOROLA = os.path.join(os.path.split(__file__)[0], 'files', 'dbc', 'comments_hex_and_motorola_converted_from_sym.dbc')
 
     maxDiff = None
 
@@ -25,6 +26,13 @@ class CanToolsConvertFullTest(unittest.TestCase):
         fn = os.path.join(tempfile.mkdtemp(), fn)
         return fn
 
+    def assertFileEqual(self, fn1, fn2):
+        with open(fn1, 'rt') as f:
+            content1 = list(f)
+        with open(fn2, 'rt') as f:
+            content2 = list(f)
+        self.assertListEqual(content1, content2)
+
     def remove_out_file(self, fn_out):
         path = os.path.split(fn_out)[0]
         shutil.rmtree(path)
@@ -33,11 +41,15 @@ class CanToolsConvertFullTest(unittest.TestCase):
 
     def test_sym_to_dbc(self):
         sym = self.SYM_FILE_COMMENTS_HEX_AND_MOTOROLA
+        dbc = self.DBC_FILE_COMMENTS_HEX_AND_MOTOROLA
         fn_out = self.get_out_file_name(sym, ext='.dbc')
         argv = ['cantools', 'convert', sym, fn_out]
 
         with mock.patch('sys.argv', argv):
             cantools._main()
+
+        fn_expected_output = dbc
+        self.assertFileEqual(fn_expected_output, fn_out)
 
         self.remove_out_file(fn_out)
 

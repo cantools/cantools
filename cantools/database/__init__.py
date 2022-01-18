cantools/cantools

commit a802630078421a75fd12d9ce9a931bc2c81f2379

sort_signals is function instead of bool [WIP]
$ mypy cantools
cantools/database/__init__.py:96: error: Argument 5 to "load" has incompatible type "Optional[Callable[[List[Signal]], None]]"; expected "bool"
Found 1 error in 1 file (checked 47 source files)

why??

cantools/database/__init__.py
@@ -78,7 +78,7 @@ def _load_file_cache(filename: StringPathLike,
                      frame_id_mask: Optional[int],
                      strict: bool,
                      cache_dir: str,
-                     sort_signals : bool,
+                     sort_signals: utils.type_sort_signals,
                      ) -> Union[can.Database, diagnostics.Database]:
     with open(filename, 'rb') as fin:
         key = fin.read()
@@ -106,7 +106,7 @@ def load_file(filename: StringPathLike,
               prune_choices: bool = True,
               strict: bool = True,
               cache_dir: Optional[str] = None,
-              sort_signals: bool = True,
+              sort_signals: utils.type_sort_signals = utils.sort_signals_by_start_bit,
               ) -> Union[can.Database, diagnostics.Database]:
     """Open, read and parse given database file and return a
     :class:`can.Database<.can.Database>` or
@@ -247,7 +247,7 @@ def load(fp: TextIO,
          frame_id_mask: Optional[int] = None,
          prune_choices: bool = True,
          strict: bool = True,
-         sort_signals: bool = True) -> Union[can.Database, diagnostics.Database]:
+         sort_signals: utils.type_sort_signals = utils.sort_signals_by_start_bit) -> Union[can.Database, diagnostics.Database]:
     """Read and parse given database file-like object and return a
     :class:`can.Database<.can.Database>` or
     :class:`diagnostics.Database<.diagnostics.Database>` object with
@@ -281,7 +281,7 @@ def load_string(string: str,
                 frame_id_mask: Optional[int] = None,
                 prune_choices: bool = True,
                 strict: bool = True,
-                sort_signals: bool = True) -> Union[can.Database, diagnostics.Database]:
+                sort_signals: utils.type_sort_signals = utils.sort_signals_by_start_bit) -> Union[can.Database, diagnostics.Database]:
     """Parse given database string and return a
     :class:`can.Database<.can.Database>` or
     :class:`diagnostics.Database<.diagnostics.Database>` object with

cantools/database/can/database.py
@@ -17,6 +17,7 @@ from .formats.dbc import DbcSpecifics
 from .internal_database import InternalDatabase
 from .message import Message
 from .node import Node
+from ..utils import type_sort_signals, sort_signals_by_start_bit
 from ...compat import fopen
 from ...typechecking import StringPathLike
 
@@ -46,7 +47,7 @@ class Database(object):
                  autosar_specifics: Optional[AutosarDatabaseSpecifics] = None,
                  frame_id_mask: Optional[int] = None,
                  strict: bool = True,
-                 sort_signals: bool = True,
+                 sort_signals: type_sort_signals = sort_signals_by_start_bit,
                  ) -> None:
         self._messages = messages or []
         self._nodes = nodes or []
@@ -319,7 +320,7 @@ class Database(object):
                                                 self._buses,
                                                 self._version,
                                                 self._dbc),
-                                                reverse_signals=self._sort_signals)
+                                                reverse_signals=bool(self._sort_signals))
 
     def as_kcd_string(self) -> str:
         """Return the database as a string formatted as a KCD file.

cantools/database/can/formats/arxml.py
@@ -13,6 +13,7 @@ from ..message import Message
 from ..node import Node
 from ..bus import Bus
 from ..internal_database import InternalDatabase
+from ...utils import sort_signals_by_start_bit
 
 LOGGER = logging.getLogger(__name__)
 
@@ -96,7 +97,7 @@ def parse_number_string(in_string, allow_float=False):
     return int(in_string, 0) # autodetect the base
 
 class SystemLoader(object):
-    def __init__(self, root, strict, sort_signals=True):
+    def __init__(self, root, strict, sort_signals=sort_signals_by_start_bit):
         self._root = root
         self._strict = strict
         self._sort_signals = sort_signals
@@ -1777,7 +1778,7 @@ REFERENCE_VALUES_XPATH = make_xpath([
 
 class EcuExtractLoader(object):
 
-    def __init__(self, root, strict, sort_signals=True):
+    def __init__(self, root, strict, sort_signals=sort_signals_by_start_bit):
         self.root = root
         self.strict = strict
         self.sort_signals = sort_signals
@@ -2100,7 +2101,7 @@ def is_ecu_extract(root):
 
     return ecuc_value_collection is not None
 
-def load_string(string, strict=True, sort_signals=True):
+def load_string(string, strict=True, sort_signals=sort_signals_by_start_bit):
     """Parse given ARXML format string.
 
     """

cantools/database/can/formats/dbc.py
@@ -33,6 +33,7 @@ from ..internal_database import InternalDatabase
 from ..environment_variable import EnvironmentVariable
 
 from .utils import num
+from ...utils import sort_signals_by_start_bit
 
 
 DBC_FMT = (
@@ -1739,7 +1740,7 @@ def get_definitions_dict(definitions, defaults):
     return result
 
 
-def load_string(string, strict=True, sort_signals=True):
+def load_string(string, strict=True, sort_signals=sort_signals_by_start_bit):
     """Parse given string.
 
     """

cantools/database/can/formats/kcd.py
@@ -18,6 +18,7 @@ from ..bus import Bus
 from ..internal_database import InternalDatabase
 from ...utils import start_bit
 from .utils import num
+from ...utils import sort_signals_by_start_bit
 
 
 LOGGER = logging.getLogger(__name__)
@@ -452,7 +453,7 @@ def dump_string(database: InternalDatabase) -> str:
     return ElementTree.tostring(network_definition, encoding='unicode')
 
 
-def load_string(string, strict=True, sort_signals=True):
+def load_string(string, strict=True, sort_signals=sort_signals_by_start_bit):
     """Parse given KCD format string.
 
     """

cantools/database/can/formats/sym.py
@@ -24,6 +24,7 @@ from ..message import Message
 from ..internal_database import InternalDatabase
 
 from .utils import num
+from ...utils import sort_signals_by_start_bit
 from ...errors import ParseError
 
 
@@ -722,7 +723,7 @@ def _load_version(tokens):
     return tokens[1][2]
 
 
-def load_string(string, strict=True, sort_signals=True):
+def load_string(string, strict=True, sort_signals=sort_signals_by_start_bit):
     """Parse given string.
 
     """

cantools/database/can/message.py
@@ -11,6 +11,8 @@ from ..utils import start_bit
 from ..utils import encode_data
 from ..utils import decode_data
 from ..utils import create_encode_decode_formats
+from ..utils import type_sort_signals
+from ..utils import sort_signals_by_start_bit
 from ..errors import Error
 from ..errors import EncodeError
 from ..errors import DecodeError
@@ -46,7 +48,7 @@ class Message(object):
                  signal_groups: Optional[List[SignalGroup]] = None,
                  strict: bool = True,
                  protocol: Optional[str] = None,
-                 sort_signals: bool = True,
+                 sort_signals: type_sort_signals = sort_signals_by_start_bit,
                  ) -> None:
         frame_id_bit_length = frame_id.bit_length()
 
@@ -66,7 +68,7 @@ class Message(object):
         self._length = length
         self._signals = signals
         if sort_signals:
-            self._signals.sort(key=start_bit)
+            sort_signals(self._signals)
 
         # if the 'comment' argument is a string, we assume that is an
         # english comment. this is slightly hacky because the

cantools/database/utils.py
@@ -4,7 +4,7 @@ import binascii
 import os.path
 import re
 from decimal import Decimal
-from typing import Dict, Union, List
+from typing import Dict, Union, List, Callable, Optional
 
 from cantools.database.can.signal import NamedSignalValue, Signal
 from cantools.typechecking import Formats
@@ -342,3 +342,8 @@ def prune_database_choices(database):
         for signal in message.signals:
             prune_signal_choices(signal)
 
+
+type_sort_signals = Optional[Callable[[List[Signal]], None]]
+
+def sort_signals_by_start_bit(signals: List[Signal]) -> None:
+	signals.sort(key=start_bit)

commit bdc543c79af2e263f3f3bb906862b584bd5876ce

bugfix: prune_choices was ignored if cache_dir was given
this is why mypy was failing since commit a802630078421a75fd12d9ce9a931bc2c81f2379

makes test implemented in commit 43e6ac831a7e4c6056e0c27a5b7304287a995e9b pass

cantools/database/__init__.py
@@ -76,6 +76,7 @@ def _load_file_cache(filename: StringPathLike,
                      database_format: Optional[str],
                      encoding: Optional[str],
                      frame_id_mask: Optional[int],
+                     prune_choices: bool,
                      strict: bool,
                      cache_dir: str,
                      sort_signals: utils.type_sort_signals,
@@ -92,6 +93,7 @@ def _load_file_cache(filename: StringPathLike,
             database = load(cast(TextIO, fin),
                             database_format,
                             frame_id_mask,
+                            prune_choices,
                             strict,
                             sort_signals)
         cache[key] = database
@@ -199,6 +201,7 @@ def load_file(filename: StringPathLike,
                                 database_format,
                                 encoding,
                                 frame_id_mask,
+                                prune_choices,
                                 strict,
                                 cache_dir,
                                 sort_signals)
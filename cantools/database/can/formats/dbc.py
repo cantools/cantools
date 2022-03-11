f9a4a1f0320010becfd63ca392c16884c8fe1a99
juleq <julian.quindt@gmail.com>
2022-03-11 01:30:41 +0100

    Revert "Update cantools/database/can/formats/dbc.py"
    This reverts commit 3f5946053566de5c02b9a99f5578247c38067625.

--- cantools/database/can/formats/dbc.py
+++ cantools/database/can/formats/dbc.py
@@ -1388,9 +1388,7 @@ def _load_messages(tokens,
             result = message_attributes['GenMsgSendType'].value
             
             # if definitions is enum (otherwise above value is maintained) -> Prevents ValueError
-            if definitions['GenMsgSendType'].choices is not None and \
-
-                int(result) in definitions['GenMsgSendType'].choices:
+            if definitions['GenMsgSendType'].choices != None:
                 # Resolve ENUM index to ENUM text
                 result = definitions['GenMsgSendType'].choices[int(result)]
         except (KeyError, TypeError):

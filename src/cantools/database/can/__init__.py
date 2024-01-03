cb97782b79a8fb8379d10620867522af16ee7612
zariiii9003 <52598363+zariiii9003@users.noreply.github.com>
2024-01-03 14:32:39 +0100

    use __all__ to mark variables for export

--- src/cantools/database/can/__init__.py
+++ src/cantools/database/can/__init__.py
@@ -1,7 +1,15 @@
-from .bus import Bus as Bus
-from .database import Database as Database
-from .message import DecodeError as DecodeError
-from .message import EncodeError as EncodeError
-from .message import Message as Message
-from .node import Node as Node
-from .signal import Signal as Signal
+__all__ = [
+    "Bus",
+    "Database",
+    "DecodeError",
+    "EncodeError",
+    "Message",
+    "Node",
+    "Signal",
+]
+
+from .bus import Bus
+from .database import Database
+from .message import DecodeError, EncodeError, Message
+from .node import Node
+from .signal import Signal

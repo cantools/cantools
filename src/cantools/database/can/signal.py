a41766a8ff78dc138f0186be0c3247563a64f6ff
Mitch Minton <mitchminton@gmail.com>
2025-12-05 21:50:00 -0600

    added signal.ldb property for determining actual dbc GIU Start bit.

--- src/cantools/database/can/signal.py
+++ src/cantools/database/can/signal.py
@@ -89,6 +89,11 @@ class Signal:
         #: Signal byte order as ``'little_endian'`` or ``'big_endian'``.
         self.byte_order: ByteOrder = byte_order
 
+        #: The least significant bit of the signal.
+        self.lsb: int = self._calc_lsb(start, length, byte_order)
+
+        #: ``True`` if the signal is signed, ``False`` otherwise. Ignore this
+
         #: ``True`` if the signal is signed, ``False`` otherwise. Ignore this
         #: attribute if :attr:`is_float` is ``True``.
         self.is_signed: bool = is_signed
@@ -153,6 +158,20 @@ class Signal:
             # assume that we have either no comment at all or a
             # multilingual dictionary
             self.comments = comment
+
+    @staticmethod
+    def _calc_lsb(start: int, length: int, byte_order: ByteOrder) -> int:
+        if byte_order == "little_endian":
+            return start
+
+        current = start
+        for _ in range(length - 1):
+            if (current % 8) == 0:
+                current += 15
+            else:
+                current -= 1
+
+        return current
 
     def raw_to_scaled(
         self, raw_value: int | float, decode_choices: bool = True

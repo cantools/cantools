9ab02475721bb832b21986963501fcfa64be2275
Andreas Lauser <andreas.lauser@mercedes-benz.com>
2026-07-24 11:09:47 +0200

    fix mypy issue
    Signed-off-by: Andreas Lauser <andreas.lauser@mercedes-benz.com>

--- src/cantools/database/conversion.py
+++ src/cantools/database/conversion.py
@@ -245,7 +245,7 @@ class NamedSignalConversion(BaseConversion):
         raw_value: float,
         decode_choices: bool = True,
     ) -> SignalValueType:
-        if decode_choices and (choice := self.choices.get(raw_value)) is not None:  # type: ignore[arg-type]
+        if decode_choices and (choice := self.choices.get(int(raw_value))) is not None:
             return choice
         return self._conversion.raw_to_scaled(raw_value, False)
 
